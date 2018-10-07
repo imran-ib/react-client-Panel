cae285483f92a1ac42c808701d8b1b07cba30fcf
imran irshad <imran.ib@live.com>
2018-10-07 21:57:12 +0300

    working in navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Bootstrap
-import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+//Components
+
+import AppNavbar from "./components/layout/AppNavbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Hello There</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <React.Fragment>
+            <Route component={AppNavbar} />
+            <div className="container">
+              <h1>Hello There</h1>
+            </div>
+          </React.Fragment>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className=" navbar navbar-expand-lg navbar-dark bg-primary">
+          <div className="container">
+            <Link to="/" className="navbar-brand">
+              Client Panel
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/">
+                    Dashboard <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
